d11d17efe53a0b8a6a982896ebb42caea0648ac7
Pranav Tadimeti <pranavtadimeti2000@gmail.com>
2020-11-05 00:06:29 +0530

    viewer complete, navigation left

--- Flask/app.py
+++ Flask/app.py
@@ -10,6 +10,8 @@ import pandas as pd
 import sql_functions as sql
 import plotly.graph_objects as go
 import base64
+import cv2 as cv
+
 server = Flask(__name__,template_folder='templates')
 
 collectionsApp = dash.Dash(
@@ -429,8 +431,8 @@ def update_output(user_name):
 
 RANGE=[0,1]
 
-im_64_1 = base64.b64encode(open(im1, 'rb').read()).decode('ascii')
-im_64_2 = base64.b64encode(open(im2, 'rb').read()).decode('ascii')
+# im_64_1 = base64.b64encode(open(im1, 'rb').read()).decode('ascii')
+# im_64_2 = base64.b64encode(open(im2, 'rb').read()).decode('ascii')
 
 doc_list = sql.get_document_by_date()
 curr_ind = 0
@@ -483,6 +485,7 @@ galleryApp.layout = html.Div(children=[
                 html.Button("prev",id="prev-btn"),
                 html.Button("next",id="next-btn"),
                 html.Br(),
+                html.Br(),
                 html.Button("approve annotation",id="approve-btn",style={"background-color":"#4CAF50",
                 "color": "white"}),
                 html.Br(),
@@ -496,51 +499,21 @@ galleryApp.layout = html.Div(children=[
     
 ])
 
-@galleryApp.callback(
-            Output("doc-img", "figure"),
-            [Input("prev-btn","n_clicks")]
-)
-
-def on_click_prev(n_clicks):
-    # return dcc.Graph(
-    # id='doc-img',
-    return {'data': [],
-        'layout': {
-            'xaxis': {
-                'range': RANGE,
-                'showgrid' : False,
-                'visible' : False,
-                'zeroline' : False
-            },
-            'yaxis': {
-                'range': RANGE,
-                'showgrid' : False,
-                'visible' : False,
-                'zeroline' : False
-            },
-            'height': 900,
-            'width' : 800,
-            'images': [{
-                'xref': 'x',
-                'yref': 'y',
-                'x': RANGE[0],
-                'y': RANGE[1],
-                'sizex': RANGE[1] - RANGE[0],
-                'sizey': RANGE[1] - RANGE[0],
-                'sizing': 'stretch',
-                'layer': 'below',
-                'source': 'data:image/jpeg;base64,{}'.format(im_64_2)
-            }],
-            'dragmode': 'select'  # or 'lasso'
-        }
-    }
-
 # @galleryApp.callback(
 #             Output("doc-img", "figure"),
-#             [Input("next-btn","n_clicks")]
+#             [Input("prev-btn","n_clicks")]
 # )
 
-# def on_click_next(n_clicks):
+# def on_click_prev(n_clicks):
+#     global curr_ind
+#     curr_ind  = max(curr_ind-1,0)
+
+#     print(doc_list[curr_ind])
+
+#     img = cv.imread(doc_list[curr_ind])
+#     h,w,_ = img.shape
+#     print(h,w)
+
 #     # return dcc.Graph(
 #     # id='doc-img',
 #     return {'data': [],
@@ -557,8 +530,8 @@ def on_click_prev(n_clicks):
 #                 'visible' : False,
 #                 'zeroline' : False
 #             },
-#             'height': 900,
-#             'width' : 800,
+#             'height': h,
+#             'width' : w,
 #             'images': [{
 #                 'xref': 'x',
 #                 'yref': 'y',
@@ -568,11 +541,59 @@ def on_click_prev(n_clicks):
 #                 'sizey': RANGE[1] - RANGE[0],
 #                 'sizing': 'stretch',
 #                 'layer': 'below',
-#                 'source': 'data:image/jpeg;base64,{}'.format(im_64_1)
+#                 'source': 'data:image/jpeg;base64,{}'.format(curr_img)
 #             }],
 #             'dragmode': 'select'  # or 'lasso'
 #         }
 #     }
+
+@galleryApp.callback(
+            Output("doc-img", "figure"),
+            [Input("next-btn","n_clicks")]
+)
+
+def on_click_next(n_clicks):
+    global curr_ind 
+    curr_ind  += 1 
+    curr_img = base64.b64encode(open(doc_list[curr_ind], 'rb').read()).decode('ascii')
+    print(doc_list[curr_ind])
+
+    img = cv.imread(doc_list[curr_ind])
+    h,w,_ = img.shape
+    print(h,w)
+
+    # return dcc.Graph(
+    # id='doc-img',
+    return {'data': [],
+        'layout': {
+            'xaxis': {
+                'range': RANGE,
+                'showgrid' : False,
+                'visible' : False,
+                'zeroline' : False
+            },
+            'yaxis': {
+                'range': RANGE,
+                'showgrid' : False,
+                'visible' : False,
+                'zeroline' : False
+            },
+            'height': h,
+            'width' : w,
+            'images': [{
+                'xref': 'x',
+                'yref': 'y',
+                'x': RANGE[0],
+                'y': RANGE[1],
+                'sizex': RANGE[1] - RANGE[0],
+                'sizey': RANGE[1] - RANGE[0],
+                'sizing': 'stretch',
+                'layer': 'below',
+                'source': 'data:image/jpeg;base64,{}'.format(curr_img)
+            }],
+            'dragmode': 'select'  # or 'lasso'
+        }
+    }
 
 
 
